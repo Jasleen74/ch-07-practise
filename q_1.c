867ba9e8e60c321caa0e83123ff24d272edde17c
Jasleen74 <preetjee@gmail.com>
2025-10-23 19:54:15 +0530

    Verifying *(array[0]+i = arr[i])

---
+++ q_1.c
@@ -0,0 +1,20 @@
+/*1. Create an array of 10 numbers. Verify using pointer arithmetic that (ptr+2) points
+to the third element where ptr is a pointer pointing to the first element of the
+array.*/
+
+#include<stdio.h>
+int main() {
+          int myarray[10] = {1,2,3,4,5,6,7,8,9,10};
+
+          int * ptr = myarray;
+
+
+        printf("The third element is : %d\n", myarray[2]);
+          printf("The value of the third element using the prt+2 is : %d", *(ptr+2));
+
+          // it proves that *(array[0]+i = arr[i])
+
+
+  
+ return 0;
+}
