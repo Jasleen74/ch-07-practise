d6caae4f612787435704ebdaad7ddc952d517f59
Jasleen74 <preetjee@gmail.com>
2025-10-23 23:50:53 +0530

    Create an array of size 3 x 10 containing multiplication tables of the numbers 2,7 and 9 respectively

---
+++ q_6.c
@@ -0,0 +1,25 @@
+/*7. Create an array of size 3 x 10 containing multiplication tables of the numbers 2,7 and 9 respectively.*/
+
+#include<stdio.h>
+int main() {
+     int array[3] [10] ;
+     int multiply[3]= {2,7,9};
+
+     for(int i = 0 ; i <3; i++){
+        for (int j = 0 ; j <10 ; j ++){
+            array[i][j] = multiply[i] *(j+1);
+           
+        }
+     }
+
+     
+     for(int i = 0 ; i <3; i++){
+        for (int j = 0 ; j <10 ; j ++){
+            printf("The table is %d\n", array[i][j]);
+        }printf("\n");
+     }
+     
+  
+  
+ return 0;
+} 
